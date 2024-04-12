7b94b2670df9c608ebc9b40f34221a11dcff357c
lpqqll <128468758+lpqqll@users.noreply.github.com>
2024-04-12 18:53:22 +0900

    Add files via upload

---
+++ wku/src/main/java/com/project24/wku/WkuApplication.java
@@ -0,0 +1,13 @@
+package com.project24.wku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WkuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WkuApplication.class, args);
+	}
+
+}
---
+++ wku/src/test/java/com/project24/wku/WkuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project24.wku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WkuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
